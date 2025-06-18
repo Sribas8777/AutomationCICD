37d18d45c77e61114e3569cd9f420dd2c7a9ff38
Sribas <dassribas44@gmail.com>
2025-06-18 09:18:21 +0530

    Upadted Standalone Test

--- src/test/java/SrijitLearning/tests/StandAloneTest.java
+++ src/test/java/SrijitLearning/tests/StandAloneTest.java
@@ -16,6 +16,8 @@ import SrijitLearning.pageobjects.LandingPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class StandAloneTest {
+	
+	//new comments
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
